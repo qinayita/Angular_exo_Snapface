ca5ec843f9ea68f27d3fc8ce4c5a6004af978c66
qinayita <domisainz94@hotmail.fr>
2023-09-06 11:45:26 +0200

    Initial commit

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+import { Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit{
+  @Input() faceSnap!: FaceSnap;
+title!: string;
+description!: string;
+createdDate!: Date;
+snaps!: number;
+imageUrl!: string;
+buttonText!: string;
+location?: string;
+
+
+ngOnInit() {
+  this.buttonText = 'Oh Snap!';
+}
+
+onSnap() {
+  if (this.buttonText === 'Oh Snap!') {
+    this.faceSnap.snaps++;
+    this.buttonText = 'Oops, unSnap!';
+  } else {
+    this.faceSnap.snaps--;
+    this.buttonText = 'Oh Snap!'
+  }
+}
+}
